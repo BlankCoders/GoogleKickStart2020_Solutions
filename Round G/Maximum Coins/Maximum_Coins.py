db5e9acb2090b4a35aca0a3dacd2d4f6c129cb15
mohinesh9999 <42772418+mohinesh9999@users.noreply.github.com>
2020-10-19 19:38:14 +0530

    Create Maximum_Coins.py
    added maximum coins problem solution  in python

---
+++ Round G/Maximum Coins/Maximum_Coins.py
@@ -0,0 +1,12 @@
+from collections import  defaultdict
+def main():
+    for _ in range(int(input())):
+        n=int(input())
+        l=[list(map(int,input().split())) for i in range(n)]
+        d=defaultdict(lambda : 0)
+        for i in range(n):
+            for j in range(n):
+                d[i-j]+=l[i][j]
+        print('Case','#'+str(_+1)+':',(d[max(d.keys(),key=lambda x:(d[x]))]))
+        
+main()
