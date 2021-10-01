e66f1d5774b6d0e7853cdfcb1c6bddcb5b349ad6
rk1091 <riya_kapoor@hotmail.com>
2021-10-02 05:13:15 +0530

    Added Friends Solution (ignore all prev attempts)
    Added the Solution of the problem 'Friends' (15pts, 23pts) of Round H 2020 - Kick Start 2020
    
    Riya Kapoor
    Rk1091

--- Round H/Friends/Riya-C++/Friends_soln.cpp
+++ Round H/Friends/Riya-C++/Friends_soln.cpp
@@ -1,38 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define send {ios_base::sync_with_stdio(false);}
-#define help {cin.tie(NULL);}
 #define f first
 #define s second
-#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
 typedef long long ll;
 typedef long double lld;
 typedef unsigned long long ull;
- 
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
-template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
-	cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
-}
-template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
-	cin >> p.first;
-	return cin >> p.second;
-}
- 
-mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-// mt19937 rng(61378913);
-/* usage - just do rng() */
- 
-void usaco(string filename) {
-  // #pragma message("be careful, freopen may be wrong")
-	freopen((filename + ".in").c_str(), "r", stdin);
-	freopen((filename + ".out").c_str(), "w", stdout);
-}
- 
-// #include <atcoder/all>
-// using namespace atcoder;
- 
 const lld pi = 3.14159265358979323846;
 const ll mod = 1000000007;
 // const ll mod = 998244353;
@@ -47,9 +19,6 @@ ll b[template_array_size];
 ll c[template_array_size];
 string s, t;
 ll ans = 0;
-
-
-
 void solve(int tc = 0) {
 	cout << "Case #" << tc + 1 << ": ";
 		
@@ -117,27 +86,8 @@ void solve(int tc = 0) {
 }
 
 int main() {
-	#ifdef Rk1091_sol
-		auto begin = std::chrono::high_resolution_clock::now();
-	#endif
-	
-	send help
- 
-	#ifndef Rk1091_sol
-		// usaco("cowpatibility");
-	#endif
-	
-	// usaco("cowland");
-	
-	// freopen("tc.cpp", "r", stdin);
- 	
+    
 	int tc = 1;
 	cin >> tc;
 	for (int t = 0; t < tc; t++) solve(t);
-	
-	#ifdef Rk1091_sol
-		auto end = std::chrono::high_resolution_clock::now();
-		cout << setprecision(4) << fixed;
-		// cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
-	#endif
-} 
+}
